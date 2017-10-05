c72877fef65e502654a3404efa500b4e05079389
Eduardo Maximiano <edu.maxi.96@gmail.com>
2017-10-04 21:41:32 -0300

    Template novo

--- PI-base/template.php
+++ PI-base/template.php
@@ -1,23 +1,29 @@
-<html>
-	<body>
-		<br><br>
-		<center>
-			<br><br><strong>Login</strong><br><br>
-			<form method="POST">
-				<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<link rel="stylesheet" type="text/css" href="template.css">
+	<meta charset="UTF-8">
+	<title>Login</title>
+</head>
+<body>
+	<div class="login">
+		<div class="login--box">
+			<form class="L_wrap" method="post">
+			<?php
 				if(isset($msg)){
 					echo "<br><b>$msg</b><br><br>";					
 				}
-				?>
-				<input type="text" name="login">
-				<br><br>
-				<input type="password" name="senha">
-				<br><br>
-				<input type="submit" value="Login" 
-				name="btnLogin">
-			
+			?>
+				<label class="L_label" for="Login">Login</label>
+				<input class="L_input" type="text" name="login" id="Login">
+				<label class="L_label" for="Senha">Senha</label>
+				<input class="L_input" type="password" name="senha" id="Senha">
+				<button class="L_submit" type="submit">LOGAR</button>
 			</form>
-		
-		</center>
-	</body>
+		</div>
+	</div>
+	<div>
+	
+	</div>
+</body>
 </html>
